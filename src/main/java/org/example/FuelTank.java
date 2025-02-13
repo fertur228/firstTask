00a283ea6a41bbd8e35d19d581f91cfecb6802b9
fertur228 <ferturferturovich@gmail.com>
2025-02-13 09:12:52 +0500

    Implemented a fuel tank

--- src/main/java/org/example/FuelTank.java
+++ src/main/java/org/example/FuelTank.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class FuelTank {
+    private int fuel = 50; // изначально 50 литров бензина
+
+    public boolean hasFuel() {
+        return fuel > 0;
+    }
+
+    public void useFuel() {
+        if (fuel > 0) {
+            fuel -= 10;
+            System.out.println("Топливо использовано! Осталось " + fuel + " литров.");
+        } else {
+            System.out.println("Топливо закончилось!");
+        }
+    }
 }
