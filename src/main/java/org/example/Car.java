c52575acbec826e395a910746b8cf45170c479fa
fertur228 <ferturferturovich@gmail.com>
2025-02-13 09:24:30 +0500

    Changed the beans, implemented them

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Car {
+    private final Engine engine;
+    private final Transmission transmission;
+    private final FuelTank fuelTank;
+
+    @Autowired
+    public Car(Engine engine, Transmission transmission, FuelTank fuelTank) {
+        this.engine = engine;
+        this.transmission = transmission;
+        this.fuelTank = fuelTank;
+    }
+
+    public void drive() {
+        if (fuelTank.hasFuel()) {
+            engine.start();
+            transmission.shiftGear();
+            fuelTank.useFuel();
+            System.out.println("Машина поехала!\n");
+        } else {
+            System.out.println("Машина не может ехать без топлива!\n");
+        }
+    }
 }
--- src/main/java/org/example/Transmission.java
+++ src/main/java/org/example/Transmission.java
@@ -1,4 +1,10 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Transmission {
+    public void shiftGear() {
+        System.out.println("⚙️ Передача переключена!");
+    }
 }
